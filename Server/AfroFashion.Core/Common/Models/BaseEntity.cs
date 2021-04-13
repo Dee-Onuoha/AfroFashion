c6f4c575eac44ab8b06a6e2b245bce9abd2fc041
Abdullahi Yusuf <abdul.benyusuf@gmail.com>
2021-04-13 17:34:01 +0100

    First commit

---
+++ Server/AfroFashion.Core/Common/Models/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AfroFashion.Core.Common.Models
+{
+    class BaseEntity
+    {
+        Guid Id { get; set; }
+    }
+}
